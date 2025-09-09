355721b6d83f1e22000ffd7b5cb4ed53ce7b65f5
Daniel Diaz <danielodiazo@gmail.com>
2025-09-09 17:59:00 +0200

    Add project files.

---
+++ TisdagFörstaExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TisdagFörstaExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
